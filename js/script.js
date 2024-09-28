a00b11f19e204f4f7e7f7f048e655631e90e06e6
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-09-29 00:05:44 +0600

    Login functionality done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mobileNumber = document.getElementById("mobile-number");
+const pinNumber = document.getElementById("pin-number");
+const loginButton = document.getElementById("login-btn");
+
+loginButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (mobileNumber.value.length < 11 || mobileNumber.value === "") {
+    alert("Number will be 11 digit");
+  } else if (pinNumber.value.length < 4 || pinNumber.value === "") {
+    alert("Pin is 4 digit number");
+  }
+  window.location.href = "home.html";
+});
